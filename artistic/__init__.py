ff2138eef35c27986636581893ee0ac4b69b4e30
Trevor Jones <trevordavidjones@gmail.com>
2021-04-28 13:22:29 -0700

    Use ENV var instead of importing app

--- artistic/__init__.py
+++ artistic/__init__.py
@@ -26,7 +26,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True, static_url_path='/artistic/static', static_folder='static')
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY=os.getenv('SECRET_KEY'),
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
         )
 
--- artistic/models/user.py
+++ artistic/models/user.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from flask import current_app as app
+import os
 from flask_sqlalchemy import SQLAlchemy
-from passlib.hash import sha256_crypt   
+from passlib.hash import sha256_crypt
 from sqlalchemy.orm import relationship
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
@@ -44,12 +44,12 @@ class User(Base):
         return f'User {email} created'
 
     def get_reset_token(self, expires_sec=1800):
-        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        s = Serializer(os.getenv('SECRET_KEY'), expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
 
     @staticmethod
     def verify_reset_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
+        s = Serializer(os.getenv('SECRET_KEY'))
         try:
             user_id = s.loads(token)['user_id']
         except:
