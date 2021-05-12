a5d80de1323e492963e0851c4ef77f18e5ae7472
Trevor Jones <trevordavidjones@gmail.com>
2021-05-11 19:39:19 -0700

    User can create a palette from the starting image

--- artistic/views/palettes.py
+++ artistic/views/palettes.py
@@ -16,23 +16,37 @@ palettes_bp = Blueprint('palettes', __name__)
 @palettes_bp.route('/images/palettes', methods=['POST'])
 @login_required
 def create():
+    def create_palette(photo_path):
+        num_palettes = int(request.form['number']) if request.form['number'] else 6
+        hex_values = photo.palette(photo_path, num_palettes=num_palettes)
+
+        palette = Palette(
+            hex_values=hex_values,
+            user_id=current_user.id,
+            name=request.form['palette_name'],
+            )
+        Path(photo_path).unlink()
+        flash('Palette created', 'success')
+        return palette
+
     if request.form['palette_name']:
-        if request.files['color_palette'].filename:
+        if 'use_starting_image' in request.form:
+            if request.form['starting_image_id']:
+                image = Image.query.get(request.form['starting_image_id'])
+                image.download(f'{PHOTO_PATH}/{image.source_name}')
+                palette = create_palette(f'{PHOTO_PATH}/{image.source_name}')
+                palette.image_id = image.id
+                palette.save()
+            else:
+                flash('Please select an image to start with', 'danger')
+        elif request.files['color_palette'].filename:
             file = request.files['color_palette']
             file_path = f'{PHOTO_PATH}/palette.png'
             file.save(file_path)
-            hex_values = photo.palette(file_path, int(request.form['number']))
-
-            palette = Palette(
-                hex_values=hex_values,
-                user_id=current_user.id,
-                name=request.form['palette_name'],
-                )
+            palette = create_palette(file_path)
             palette.save()
-            flash('Palette created', 'success')
-            Path(file_path).unlink()
         else:
-            flash('Please upload an image', 'danger')
+            flash('Please upload an image or select an image to start with', 'danger')
     else:
         flash('Please name your palette', 'danger')
 
